bd8b420fde4509c20a31d2a7f93e5060a60a80db
MytyMyty <ma.gnecco@duocuc.cl>
2024-11-25 15:12:38 -0300

    NuevaRuta1.2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsApiKey: "AIzaSyAsYrv2invX5PLg8eBwUPQAi7Uvi2mC1NQ"};
+  apiURL:"http://localhost:3301",
+  apiURL2:"http://localhost:3300",
+  googleMapsApiKey: 'AIzaSyAsYrv2invX5PLg8eBwUPQAi7Uvi2mC1NQ'};
