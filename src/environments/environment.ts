a3547c790783b273f150f6adb5fbf829702a4ebb
MytyMyty <ma.gnecco@duocuc.cl>
2024-11-28 16:02:36 -0300

    NuevaRuta1.81

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiURL:"http://localhost:3301",
   apiURL2:"http://localhost:3300",
   googleMapsApiKey: 'AIzaSyAsYrv2invX5PLg8eBwUPQAi7Uvi2mC1NQ'
